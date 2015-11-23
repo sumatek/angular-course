1d8646ce515d670cbe95b83b094d64bedc96d7a4
sumatek <sumate@dtac.co.th>
2015-11-23 12:01:23 +0700

    Initial controller

--- 2_form_and_controller/main.js
+++ 2_form_and_controller/main.js
@@ -1,1 +1,12 @@
+var myApp = angular.module('myApp', []);
 
+myApp.controller('FormController', function($scope){
+	$scope.formData = {
+		name: "iPalm",
+		email: "kit.sumate@gmail.com"
+	};
+
+	$scope.register = function() {
+		alert($scope.formData.name);
+	};
+})
